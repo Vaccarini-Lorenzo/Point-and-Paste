b94ad26ceb452c71ada0ab8faa1f91e051b2d9ee
Vaccarini-Lorenzo <lorenzomaria.vaccarini@mail.polimi.it>
2022-08-17 19:27:48 +0200

    smarter selection: collapse pixels with rgb values inside a certain delta

--- PP/header.swift
+++ PP/header.swift
@@ -10,6 +10,13 @@ import CoreGraphics
 import ApplicationServices
 import AppKit
 
+struct pxl: Hashable{
+    let hex: String
+    let r: UInt8
+    let g: UInt8
+    let b: UInt8
+}
+
 func CGInit(){
     CGMainDisplayID();
 }
@@ -53,8 +60,11 @@ func getIdFromMouseCoordinates(_ mouse: CGPoint) -> CGDirectDisplayID{
 
 
 func getHexString(r: UInt8, g: UInt8, b:UInt8) -> String {
+    let r_hex = r < 16 ? String(format: "0%X", r) : String(format: "%X", r)
+    let g_hex = r < 16 ? String(format: "0%X", g) : String(format: "%X", g)
+    let b_hex = r < 16 ? String(format: "0%X", b) : String(format: "%X", b)
 
-    return String(format: "%X%X%X", r, g, b)
+    return r_hex + g_hex + b_hex
 }
 
 func getImage(id: CGDirectDisplayID, size: Int, mouse: CGPoint) -> CGImage {
@@ -70,21 +80,51 @@ func getImage(id: CGDirectDisplayID, size: Int, mouse: CGPoint) -> CGImage {
     return CGDisplayCreateImage(id, rect: rect)!
 }
 
+func getRelatedPixel(p: pxl, dict: [pxl: Int]) -> (pxl, Int)?{
+    for item in dict {
+        
+        let rDelta = (item.key.r < 245 && item.key.r > 9) ? 10 : min(255 - item.key.r, item.key.r)
+        
+        let gDelta = (item.key.g < 245 && item.key.g > 9) ? 10 : min(255 - item.key.g, item.key.g)
+        
+        let bDelta = (item.key.b < 245 && item.key.b > 9) ? 10 : min(255 - item.key.b, item.key.b)
+        
+        let rRange = item.key.r - rDelta ... item.key.r + rDelta
+        let gRange = item.key.g - gDelta ... item.key.g + gDelta
+        let bRange = item.key.b - bDelta ... item.key.b + bDelta
+        if rRange.contains(p.r) && gRange.contains(p.g) && bRange.contains(p.b) {
+            return item
+        }
+    }
+    return nil;
+}
+
 func votePixel(rawData: [CUnsignedChar], size: Int) -> String {
-    var pixelDict: [String : Int] = [:]
+    var pixelDict: [pxl : Int] = [:]
     // RGBA
     let shift = 4
-    for index in 0..<size*size {
-        let r = rawData[index * shift]
-        let g = rawData[index * shift + 1]
-        let b = rawData[index * shift + 2]
-        
-        let hex = getHexString(r: r, g: g, b: b)
-        if pixelDict[hex] == nil {
-            pixelDict[hex] = 1
-        } else {
-            let value = pixelDict[hex]!
-            pixelDict[hex] = value + 1
+    let bytesPerPixel = 4
+    let bytesPerRow = bytesPerPixel * Int(size)
+    let bitsPerComponent = 8
+    
+    for w in 0..<size {
+        for h in 0..<size {
+            var base = h * bytesPerRow + w * shift
+            let r = rawData[base]
+            let g = rawData[base + 1]
+            let b = rawData[base + 2]
+            
+            print(r, g, b)
+            
+            let hex = getHexString(r: r, g: g, b: b)
+            let p = pxl(hex: hex, r: r, g: g, b: b)
+            let relatedPixel = getRelatedPixel(p: p, dict: pixelDict)
+            
+            if pixelDict.isEmpty || relatedPixel == nil {
+                pixelDict[p] = 1
+            } else {
+                pixelDict[relatedPixel!.0] = relatedPixel!.1 + 1
+            }
         }
     }
     //print(pixelDict)
@@ -92,15 +132,21 @@ func votePixel(rawData: [CUnsignedChar], size: Int) -> String {
         a.value < b.value
     }
     if maxPixel != nil {
-        return maxPixel!.key
+        return maxPixel!.key.hex
     }
+    //print(pixelDict)
     return "ERROR"
 
 }
 
 func getPixel(id: CGDirectDisplayID, mouse: CGPoint) -> String {
+    
     // 10px x 10px
-    let size = 5
+    var size = 5
+    
+    if CommandLine.arguments.count > 1 {
+        size = Int(CommandLine.arguments[1])!
+    }
     
     let imageRef = getImage(id: id, size: size, mouse: mouse)
 
---
+++ README.md
@@ -0,0 +1,2 @@
+# Point & Paste
+Point & Paste is a simple macOS tool that makes the research for the right color easier: Just point your cursor to a color you like and copy its hex code to your clipboard.
